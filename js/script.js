119e7fddadb4b0854eaa60e9edbcc8ac99555590
EvilTwin01 <mohdariffin.work@gmail.com>
2017-11-04 16:01:25 +0800

    responsive update 1

--- js/script.js
+++ js/script.js
@@ -15,4 +15,21 @@ $(document).ready(function() {
             icon.removeClass('ion-close-round');
         }        
     });
+    
+    // ===== Scroll to Top ==== 
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+            $('#return-to-top').fadeIn(200);    // Fade in the arrow
+        } else {
+            $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+        }
+    });
+    
+    $('#return-to-top').click(function() {      // When arrow is clicked
+        $('body,html').animate({
+            scrollTop : $('.address').offset().top                      // Scroll to top of body
+        }, 1000);
+    });
+    
 });
+
